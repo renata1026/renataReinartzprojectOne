eac4d94a456a44fb8cc986003219a8250078cd2a
Renata Reinartz <rcreinartz@gmail.com>
2022-12-03 18:25:22 -0700

    nav bar & blog

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger=document.getElementById("hamburger")
+hamburger.addEventListener("click", ()=>{
+
+const mobileNav = document.querySelector(".mobileNavMenu")
+mobileNav.classList.toggle("none")
+
+})
